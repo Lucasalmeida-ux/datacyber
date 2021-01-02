790c91e3f6c9b70d8efa341cae0f7393de4af1b1
Lucas Almeida <lucas@w3mind.com.br>
2021-01-02 14:48:39 -0300

    alterando cor do tema

--- pages/components/Banner.js
+++ pages/components/Banner.js
@@ -1,6 +1,6 @@
 export default function Banner() {
     return (
-    <div className="has-background-success-light">
+    <div className="has-background-light">
         <div className="container">
             <a href="https://pag.ae/7WxbMVsb2/button" target="_blank" title="Pagar com PagSeguro" className="banner"><img src="banner.png" width="200" /></a>
         </div>
