1780e56c265b14af969b17eb8a1afec9c9be82f9
Lucas Almeida <lucas@w3mind.com.br>
2021-01-02 13:33:38 -0300

    tentando corrigir deploy

--- pages/components/card.js
+++ pages/components/card.js
@@ -1,14 +1,14 @@
 export default function CardCollapseble({ children, title}) {
-  var id = title.replace(/\s/g, '');
+  const idBox = title.replace(/\s/g, '');
   return (
     <div id="card">
-      <input type="checkbox" id={id} />
-      <label className="collapse" for={id}>
+      <input type="checkbox" id={idBox} />
+      <label className="collapse" for={idBox}>
         <h3 className="has-background-white-ter py-2 px-3 my-2 box">{title}</h3>
         <div className="details has-background-white-bis px-3 py-3">{children}</div>
       </label>
       
     </div>
 
-  )
+  );
 }
