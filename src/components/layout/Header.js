3af875a4eb1308eb6254755156ba677ca192f796
Lucasalmeida-ux <lucas@w3mind.com.br>
2021-03-09 21:16:28 -0300

    corrigindo responsividade do menu

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
         setToogleMenu(!toogleMenu);
     }
     return (
-        <header className="flex justify-between bg-white p-2 shadow-md">
+        <header className="justify-between bg-white p-2 shadow-md">
             <div className="inline-block p-1 md:p-2">
                 <a className="pr-3 md:hidden ">
                     <FaBars onClick={openMenu} className="text-gray-300 text-lg inline-block" />
@@ -17,25 +17,25 @@ export default function Header() {
                 <Link href="/"><a><img src="/logo.png" className="w-25 h-7 md:w-25 md:h-8 inline-block" alt="BETBOL logo" /></a></Link>
 
             </div>
-            <div>
+            
                 <nav className={`${toogleMenu ? 'block' : 'hidden'} bg-white md:bg-transparent divide-y-2 md:divide-none z-10 fixed md:static top-10 left-0 md:inline-block md:p-3`}>
-                    <item className="w-screen bg-white block md:inline">
+                    
                         <Link href="/solutions/comercio">
-                            <a className="transition-colors text-gray-400 hover:text-blue-400 mr-5 uppercase text-sm md:font-bold px-4">Soluções para Comércios</a>
+                            <a className="block sm:inline transition-colors text-gray-400 hover:text-blue-400 mr-5 md:mr-1 md:mr-5 uppercase text-sm md:text-xs lg:text-sm md:font-bold px-4 md:px-1 lg:px-4">Soluções para Comércios</a>
                         </Link>
-                    </item>
-                    <item className="w-screen bg-white block md:inline">
+                    
+                    
                         <Link href="/solutions/distribuidor">
-                            <a className="transition-colors text-gray-400 hover:text-blue-400 mr-5 uppercase text-sm md:font-bold px-4">Soluções para Distribuidoras e Indústrias</a>
+                            <a className="block sm:inline ransition-colors text-gray-400 hover:text-blue-400 mr-5 md:mr-1 md:mr-5 uppercase text-sm md:text-xs lg:text-sm md:font-bold px-4 md:px-1 lg:px-4">Soluções para Istribuidoras e Indústrias</a>
                         </Link>
-                    </item>
-                    <item className="w-screen bg-white block md:inline">
+                    
+                    
                         
                             <a href="http://eepurl.com/hplomL" target="_BLANK" className="transition-colors text-gray-400 hover:text-white uppercase mr-5 md:py-2 px-4 md:font-semibold rounded-md md:bg-gray-100 hover:bg-blue-400">Orçamento</a>
                         
-                    </item>
+                    
                 </nav>
-            </div>
+            
         </header>
     )
 }
